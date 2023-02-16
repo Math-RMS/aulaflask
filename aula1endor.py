4cc1660a69f9d523fd522464d788ca654a4ed65a
EC2 Default User <ec2-user@ip-172-31-33-68.ec2.internal>
2023-02-16 01:01:17 +0000

    aulaflask

---
+++ aula1endor.py
@@ -0,0 +1,15 @@
+x = int(imput("Digite o primeiro número: "))
+y = int(imput("Digite o segundo número: "))
+z = int(input("Digite o terceiro número: "))
+
+if x > y  and x > z:
+    result = "x é maior número"
+elif y > x and y > z:
+    result = "y é maior número"
+elif z > x and z > y:
+    result = "z é maior número"
+else 
+    result = "Há números iguais"
+    
+
+
---
+++ aula2andor.py
@@ -0,0 +1,7 @@
+idade = int(input("Digite a sua idade: "))
+dinheiro = int(input("Digite a quantidade de dinheiro que você tem: "))
+
+if idade >= 18 or dinheiro >=50
+    print("Voc~e pode entrar na festa")
+else:
+    print("Você não pode entar na festa")
